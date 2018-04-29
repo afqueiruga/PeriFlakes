8fe8aed6090b91da065d334ba3321e3b79d2c094
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2018-04-29 16:31:03 -0700

    Scratch of the kernels

---
+++ src/kernels.py
@@ -0,0 +1,86 @@
+from popcorn import *
+# This is going to be a variable length kernel, but it's a little funky.
+# It has both Particle and Bond DOFs, and in the edge they'll be ordered like this
+#
+# P0  P1 P2 P3 ... Pn  B1 B2 B3 ... Bn 
+#
+# to represent this
+#
+# (P2)~~B2~~ ((P0)) ~~B1~~(P1)
+#                `.
+#                  `~B3~~(P3)
+#
+gdim = 2
+II = Symbol('i')
+JJ = Symbol('j')
+l_edge = Symbol("l_edge")
+Npart = (l_edge+1)/2
+
+# Typedefs
+PointVec = DofSpace(gdim, 0,Npart,1)
+BondSca = DofSpace(1, Npart, l_edge)
+PointSca = DofSpace(1, 0, Npart)
+PointTen = DofSpace(gdim*gdim, 0,Npart)
+Param = DofSpace(1, 0,1)
+
+# Inputs
+i_y = Input("y",PointVec)
+i_x = Input("x",PointVec)
+i_p = Input("p",PointSca)
+i_r = Input("r",PointSca)
+i_alpha = Input("alpha",BondSca)
+i_pfrac= Input("pfrac",BondSca)
+# Parameter inputs
+i_delta = Input("delta", Param)
+i_E = Input("p_E", Param)
+i_nu = Input("p_nu", Param)
+i_Vol = Input("p_Vol", Param)
+# Outputs
+o_F  = Output("F",  [PointVec], 1)
+o_K  = Output("K",  [PointVec], 2 )
+
+# Do some symbolic math
+c_K = i_E[0]/(3.0-6.0*i_nu[0])
+c_G = i_E[0]/(2.0+2.0*i_nu[0])
+y0,yI,yJ = i_y.Vertex_Handles(0,II,JJ)
+x0,xI,xJ = i_y.Vertex_Handles(0,II,JJ)
+alpha_I = i_alpha.Vertex_Handle(II-1)[0,0]
+pfrac_I = i_pfrac.Vertex_Handle(II-1)[0,0]
+alpha_J = i_alpha.Vertex_Handle(JJ-1)[0,0]
+pfrac_J = i_pfrac.Vertex_Handle(JJ-1)[0,0]
+
+def norm(a):
+    return sqrt((a.T*a)[0,0])
+rxI = xI-x0
+ryI = yI-y0
+rxabs = norm(rxI)
+ryabs = norm(ryI)
+xn = rxI/rxabs
+yn = ryI/ryabs
+strainI = (ryabs-rxabs)
+
+class StateBased():
+    def __init__(self, name, w_of_r):
+        self.name = name
+        self.w_of_r = w_of_r
+        self.w0I = w_of_r( rxabs )
+    def m_expr(self):
+        return self.w0I* alpha_I * rxabs*rxabs
+    def theta_expr(self):
+        return gdim/m_sym *  self.w0I*alpha_I * ((ryabs-rxabs)* rxabs)
+    def force(self):
+        A =  alpha_I * self.w0I/m_sym *(  ( 2.0*(c_K-c_G/3.0) * th )* rxabs
+                                          + 8.0*c_G * (strainI - th*rxabs/3.0) )
+        return A * xn
+    def _sum_prgm(self, var, expr):
+        return [
+            Loop(II,1,Npart,[
+                Asgn(var, expr, "+=")
+                ])
+            ]
+    def _tang_prgm(self, var, expr):
+        return []
+    def kernel(self):
+        prgm = []
+        return Kernel(self.name, prgm)
+
