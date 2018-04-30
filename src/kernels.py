941d917c0b72137ef9bfa9d24a695f2f4700f64e
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2018-04-29 17:22:33 -0700

    Fleshing out the first kernel

--- src/kernels.py
+++ src/kernels.py
@@ -43,7 +43,7 @@ o_K  = Output("K",  [PointVec], 2 )
 c_K = i_E[0]/(3.0-6.0*i_nu[0])
 c_G = i_E[0]/(2.0+2.0*i_nu[0])
 y0,yI,yJ = i_y.Vertex_Handles(0,II,JJ)
-x0,xI,xJ = i_y.Vertex_Handles(0,II,JJ)
+x0,xI,xJ = i_x.Vertex_Handles(0,II,JJ)
 alpha_I = i_alpha.Vertex_Handle(II-1)[0,0]
 pfrac_I = i_pfrac.Vertex_Handle(II-1)[0,0]
 alpha_J = i_alpha.Vertex_Handle(JJ-1)[0,0]
@@ -59,6 +59,9 @@ xn = rxI/rxabs
 yn = ryI/ryabs
 strainI = (ryabs-rxabs)
 
+m     = PopcornVariable("m",1,1)
+theta = PopcornVariable("theta",1,1)
+
 class StateBased():
     def __init__(self, name, w_of_r):
         self.name = name
@@ -67,13 +70,14 @@ class StateBased():
     def m_expr(self):
         return self.w0I* alpha_I * rxabs*rxabs
     def theta_expr(self):
-        return gdim/m_sym *  self.w0I*alpha_I * ((ryabs-rxabs)* rxabs)
+        return gdim/m[0] *  self.w0I*alpha_I * ((ryabs-rxabs)* rxabs)
     def force(self):
-        A =  alpha_I * self.w0I/m_sym *(  ( 2.0*(c_K-c_G/3.0) * th )* rxabs
-                                          + 8.0*c_G * (strainI - th*rxabs/3.0) )
+        A =  alpha_I * self.w0I/m[0] *(  ( 2.0*(c_K-c_G/3.0) * theta[0] )* rxabs
+                                          + 8.0*c_G * (strainI - theta[0]*rxabs/3.0) )
         return A * xn
     def _sum_prgm(self, var, expr):
         return [
+            var,
             Loop(II,1,Npart,[
                 Asgn(var, expr, "+=")
                 ])
@@ -81,6 +85,63 @@ class StateBased():
     def _tang_prgm(self, var, expr):
         return []
     def kernel(self):
-        prgm = []
-        return Kernel(self.name, prgm)
+        prgm = self._sum_prgm(m, Matrix([self.m_expr()]) )
+        theta_expr = self.theta_expr()
+        prgm += self._sum_prgm(theta, Matrix([theta_expr]))
 
+        theta_Dy0 = PopcornVariable("theta_Dy0",gdim,1)
+        prgm += self._sum_prgm( theta_Dy0, Matrix([theta_expr]).jacobian(y0).T )
+        
+        theta_exprJ = theta_expr.subs([ (yI,yJ),(xI,xJ), (alpha_I,alpha_J)])
+        theta_DyJ = Matrix([theta_exprJ]).jacobian(yJ).T
+
+        tA = self.force()
+
+        # And tangent matrices
+        tA_Dtheta = tA.diff(theta[0])
+        tAI_Dy0 =  tA.jacobian(y0) + tA_Dtheta*theta_Dy0.T 
+        tAI_DyI =  tA.jacobian(yI)
+        tAI_DyJ = tA_Dtheta * theta_DyJ.T
+
+        
+        prgm += [
+            Loop(II,1,Npart,[
+                Asgn(o_F.View((0,)), tA,"+="),
+                Asgn(o_F.View((gdim*II,)), tA,"-="),                
+            ]),
+            Loop(II,1,Npart,[
+                Asgn(o_K.View((0,0)),             tAI_Dy0,"+="),
+                Asgn(o_K.View((0,gdim*II)),       tAI_DyI,"+="),
+                Asgn(o_K.View((gdim*II,0)),       tAI_Dy0,"-="),
+                Asgn(o_K.View((gdim*II,gdim*II)), tAI_DyI,"-="),
+            ])
+        ]
+        print prgm
+        return Kernel(self.name, listing=prgm)
+
+
+
+formulations = {
+    'peri_ouchi' : StateBased,
+    # 'peri_Oterkus2_strain': Oterkus,
+    # 'peri_Fbased_strain':Fbased
+}
+delta = i_delta[0]
+weight_funcs = {
+    'const' : lambda r : Piecewise( (1.0    , Le(r,delta)), (0.0,True)),
+    'inv'   : lambda r : Piecewise( (delta/r, Le(r,delta)), (0.0,True)),
+    'linear': lambda r : Piecewise( ((1.0-r / delta)**1.0, Le(r,delta)), (0.0,True)),
+    'quadr' : lambda r : Piecewise( ((1.0-r / delta)**2.0, Le(r,delta)), (0.0,True)),
+    'cubic' : lambda r : Piecewise( ((1.0-r / delta)**3.0, Le(r,delta)), (0.0,True)),
+    #'cubicspline':lambda r : Piecewise(
+    #    (2.0/3.0 - 4.0*r**2 + 4.0* r**3, Le(r,0.5)  ),
+    #    (4.0/3.0 - 4.0*r + 4.0*r**2 - 4.0/3.0*r**3, Lt(r,1.0)),
+    #    (0.0,True) ),
+    'quarticA':lambda r: Piecewise( (1.0 - 6.0*(r/delta)**2 + 8.0*(r/delta)**3 - 3.0*(r/delta)**4, Le(r,delta)), (0.0,True))
+
+}
+    
+Pdbg = StateBased("silling",weight_funcs['cubic'])
+Pdbg.kernel()
+
+Husk('peridynamics')
