4829d2d08ff1b44d02a6ba62b35cd50c0ecc528e
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2018-05-29 21:40:15 -0700

    Added in the analytical solution and a fix to evaluate it on the crack

--- fem/fem_crack.py
+++ fem/fem_crack.py
@@ -4,6 +4,7 @@ import os
 import tempfile
 import shutil
 import numpy as np
+import timeit
 
 # This script calls gmsh each time, but needs v3+
 #gmshloc = "gmsh"
@@ -11,15 +12,43 @@ gmshloc = "~/Documents/gmsh-3.0.4-Linux/bin/gmsh"
 # gmshloc = "/Applications/Gmsh.app/Contents/MacOS/gmsh"
 dbloc = "femdata.db"
 
-# Switch to a scratch spack and make a new mesh
+# Switch to a scratch space and make a new mesh
 sdb = SimDataDB(dbloc)
 geo = os.path.abspath("crack.geo")
+onum = 0
+# Some global properties
+E = 1.0
+nu= 0.25
+H = 2.0
+P = 0.1
+a = 0.1
+C = 0.0 # for the analytical solution
+
+# We use the analytical solution
+class LFM_Anal(Expression):
+    def eval(self, value, x):
+        Zhat = lambda z : np.sqrt( -a**2 + z**2.0 ) - z - C *z
+        Z = lambda z : z/np.sqrt(-a**2 + z**2.0 ) - 1.0 - C
+        ux = lambda z : P/(2.0*E/(2.0*(1.0-nu))) * (
+            (2.0-4.0*nu)/2.0 * np.real(Zhat(z)) - np.imag(z) * np.imag(Z(z)))
+        uy = lambda z : P/(2.0*E/(2.0*(1.0-nu))) * (
+            (4.0-4.0*nu)/2.0 * np.imag(Zhat(z)) - np.imag(z) * np.real(Z(z)))
+        #ux = lambda z : P/(2.0*E/(2.0*(1.0-nu))) * (
+        #    (2.0-4.0*nu)/2.0 * np.imag(Zhat(z)) - np.imag(z) * np.real(Z(z)))
+        #uy = lambda z : P/(2.0*E/(2.0*(1.0-nu))) * (
+        #    (4.0-4.0*nu)/2.0 * np.real(Zhat(z)) - np.imag(z) * np.imag(Z(z)))
+        value[0] = np.sign(x[0])*ux( np.abs(x[0])+1.0e-12 + 1j*(np.abs(x[1])+1.0e-12) )
+        value[1] = np.sign(x[1])*uy( np.abs(x[0])+1.0e-12 + 1j*(np.abs(x[1])+1.0e-12) )
+
+    def value_shape(self):
+        return (2,)
+expr_lfm_anal = LFM_Anal(degree=0)
 
 @sdb.Decorate("lfm",
-              [("clscale","FLOAT")],
-              [("x","array"),("u","array")])
-def sim(clscale):
-    global dx,ds
+              [("clscale","FLOAT"),("poly","INT")],
+              [("h","FLOAT"),("error","FLOAT"),("time","FLOAT"),("x","array"),("u","array")])
+def sim(clscale,poly):
+    global dx,ds, onum
     tmpdir = tempfile.mkdtemp()
     #os.chdir(tmpdir)
     os.system("cp "+geo+" "+tmpdir+"/crack.geo")
@@ -35,29 +64,47 @@ def sim(clscale):
     dx = dx(subdomain_data=cellids)
     ds = ds(subdomain_data=facetids)
     
-    V = VectorFunctionSpace(mesh,"CG",1)
+    V = VectorFunctionSpace(mesh,"CG",poly)
     u,tu,Du = Function(V,name='u'),TestFunction(V),TrialFunction(V)
     
     n = FacetNormal(mesh)
-    p = 1.0
-    K = 1.0
-    G = 1.0
-    sigma = (K-2.0/3.0*G)*tr(sym(grad(u)))*Identity(2) + 2.0*G*sym(grad(u))
-    f_R = inner(grad(tu),sigma)*dx - inner(tu,-n*p)*ds(2) - inner(tu,-n*p)*ds(4)
+    p = 0.1
+    K = E/(3.0-6.0*nu)
+    G = E/(2.0+2.0*nu)
+    Dsigma = (K-2.0/3.0*G)*tr(sym(grad(Du)))*Identity(2) + 2.0*G*sym(grad(Du))
+    a = inner(grad(tu),Dsigma)*dx
+    L = - inner(tu, n*p)*ds(2) - inner(tu, n*p)*ds(4)
     bcs = [
-        DirichletBC(V,Constant((0.0,0.0)), facetids,3)
+        DirichletBC(V,expr_lfm_anal, facetids,3)
     ]
-    solve(f_R==0, u, bcs=bcs)
-    
+    def work():
+        solve(a==L, u, bcs=bcs)
+    runtime = timeit.timeit(work, number=1)
     shutil.rmtree(tmpdir)
 
-    return mesh.coordinates(), u.vector().get_local()
+    for l in facets(mesh):
+        if facetids[l.index()]==2:
+            mesh.coordinates()[l.entities(0),:] -= 1.0e-8
+        if facetids[l.index()]==4:
+            mesh.coordinates()[l.entities(0),:] += 1.0e-8
+
+    # from IPython import embed ; embed()
+    
+    File('outs/u_{0}.pvd'.format(onum)) << u
+    en = project(u-expr_lfm_anal, V)
+    en.rename('e','e')
+    File('outs/en_{0}.pvd'.format(onum)) << en
+    onum += 1
+    error = np.sqrt( assemble( inner( u-expr_lfm_anal, u-expr_lfm_anal)*dx ) \
+                     / assemble( inner(expr_lfm_anal, expr_lfm_anal )*dx(mesh) ) )
+    return mesh.rmin(), error, runtime, mesh.coordinates(), u.vector().get_local()
 
 import multiprocessing as multi
 import itertools
-resolutions = np.linspace(0.5,2.0,10)
+resolutions = np.linspace(0.25,2.0,10)
+orders = [1,2,3]
 def f(t):
     print t
     return sim(*t)
 poo = multi.Pool(processes=3)
-map(f, itertools.product( resolutions) ) 
+map(f, itertools.product( resolutions,orders) ) 
