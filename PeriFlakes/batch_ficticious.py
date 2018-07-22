7512db3ab4b00094daa3a5ac6251d6aa4080f422
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2018-07-21 20:59:29 -0700

    Illustrating those surface corrections

--- PeriFlakes/batch_ficticious.py
+++ PeriFlakes/batch_ficticious.py
@@ -1,20 +1,20 @@
 from SimDataDB import *
 from PeriBlock import PeriBlock
 
-NS = [10,15,20,25,35,50,75,100]
+NS = [10,15,20,25,35,50,75,100,125,150,175,200]
 RFS = [1.5]
-methods = ['Silling','Fbased']#,'Fstab_Littlewood','Fstab_Silling']
+methods = ['Silling','Fbased','Fstab_Silling','Oterkus2']#,'Fstab_Littlewood','Fstab_Silling']
 weights = ['cubic','const']#['const','inv','linear','quadr','cubic','quarticA']
 surface_methods = ['none','trivial','bobaru','both']
-sdb = SimDataDB('results_ficticious.db')
+sdb = SimDataDB('results_ficticious_nu0.db')
 
 E = 1.0
-nu = 0.25
+nu = 0.0
 
 @sdb.Decorate('uniaxial',
               [('surface','TEXT'),('method','TEXT'),('weight','TEXT'),('RF','FLOAT'),('N','INT')],
               [('x','array'),('u','array')])
-def sim(surf,met,wei,RF,N):
+def sim_uni(surf,met,wei,RF,N):
     print "Solving", surf, " ", met," ",wei," ",RF," ",N
     global onum
     PB = PeriBlock(1.0, N, RF*2.0/float(N), E,nu, ficticious=surf!="none")
@@ -25,9 +25,43 @@ def sim(surf,met,wei,RF,N):
 
     u=PB.solve(met,wei,fictmet = surf)
     return PB.x,u
+
+@sdb.Decorate('shear',
+              [('surface','TEXT'),('method','TEXT'),('weight','TEXT'),('RF','FLOAT'),('N','INT')],
+              [('x','array'),('u','array')])
+def sim_shear(surf,met,wei,RF,N):
+    print "Solving", surf, " ", met," ",wei," ",RF," ",N
+    global onum
+    PB = PeriBlock(1.0, N, RF*2.0/float(N), E,nu, ficticious=surf!="none")
+    if surf=="none":
+        PB.setbcs([(PB.right,1),(PB.left,1),(PB.bottom,0)], [(PB.top,[1,0])])
+    else:
+        PB.setbcs([(PB.right,1),(PB.left,1),(PB.bottom,0)], [(PB.ftop,[1,0])])
+
+    u=PB.solve(met,wei,fictmet = surf)
+    return PB.x,u
+
+
+@sdb.Decorate('isotropic',
+              [('surface','TEXT'),('method','TEXT'),('weight','TEXT'),('RF','FLOAT'),('N','INT')],
+              [('x','array'),('u','array')])
+def sim_iso(surf,met,wei,RF,N):
+    print "Solving", surf, " ", met," ",wei," ",RF," ",N
+    global onum
+    PB = PeriBlock(1.0, N, RF*2.0/float(N), E,nu, ficticious=surf!="none")
+    if surf=="none":
+        PB.setbcs([(PB.left,0),(PB.bottom,1)], [(PB.top,[0,1]),(PB.right,[1,0])])
+    else:
+        PB.setbcs([(PB.left,0),(PB.bottom,1)], [(PB.ftop,[0,1]),(PB.fright,[1,0])])
+
+    u=PB.solve(met,wei,fictmet = surf)
+    return PB.x,u
+
+
 for N in NS:
     for RF in RFS:
         for met in methods:
             for wei in weights:
                 for surf in surface_methods:
-                    sim(surf,met,wei,RF,N)
+                    for sim in [sim_uni,sim_shear,sim_iso]:
+                        sim(surf,met,wei,RF,N)
